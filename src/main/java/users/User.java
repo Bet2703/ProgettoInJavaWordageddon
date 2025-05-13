0aea87a58e6b57072acbb053e4ed089e6c2236b2
Benedetta <Benedetta@172.19.134.53>
2025-05-13 14:03:59 +0200

    Prime classi

--- src/main/java/users/User.java
+++ src/main/java/users/User.java
@@ -5,10 +5,55 @@
  */
 package users;
 
+import java.util.Objects;
+
 /**
  *
  * @author Benedetta
  */
-public interface User {
+public abstract class User {
+    private final String username;
+    private final String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean checkPassword(String input) {
+        return password.equals(input);
+    }
     
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.username);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        if (!Objects.equals(this.username, other.username)) {
+            return false;
+        }
+        return true;
+    }    
 }
