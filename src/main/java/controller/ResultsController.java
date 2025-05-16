22592293dcb72889aa59162c487e127c6ea28244
andre <andre@172.19.144.144>
2025-05-16 13:06:16 +0200

    result contnroller

--- src/main/java/controller/ResultsController.java
+++ src/main/java/controller/ResultsController.java
@@ -1,6 +1,21 @@
 package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 /**
  * Displays game results and performance feedback.
  */
 public class ResultsController {
+
+    @FXML
+    private Label correctCountLabel;
+    @FXML
+    private Button btnHome;
+
+    @FXML
+    private void onBackToMenu(ActionEvent event) {
+    }
 }
